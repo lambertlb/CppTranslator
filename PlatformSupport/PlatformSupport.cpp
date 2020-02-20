c9bcba91cbbe04bfee03d795edad8a0a902584d6
LLambert <Leon.Lambert@honeywell.com>
2020-02-20 07:28:17 -0600

    add comments

--- PlatformSupport/PlatformSupport.cpp
+++ PlatformSupport/PlatformSupport.cpp
@@ -1,17 +1,68 @@
+// Copyright (c) 2019 LLambert
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 #include "PlatformSupport.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef MEMORY_SIZE
+#define		MEMORY_SIZE 32767
+#endif // !MEMORY_SIZE
+
+uint32_t memorySize;
+uint32_t memoryUsed;
+char* currentMemory = nullptr;
+char* memory = nullptr;
+
 DLL_PLATFORMSUPPORT_EXPORTS void* CDECL my_operator_new_replacement(size_t size)
 {
-	void* memory = malloc(size);
-	if (memory != nullptr)
-		memset(memory, 0, size);
-	return(memory);
+	uint32_t required = (uint32_t)size;
+	required = Align(required, sizeof(double));
+	if (memoryUsed + required > memorySize)
+		return(nullptr);
+	memoryUsed += required;
+	char* ret = currentMemory;
+	currentMemory += required;
+	memset(ret, 0, size);
+	return(ret);
+	//void* memory = malloc(size);
+	//if (memory != nullptr)
+	//	memset(memory, 0, size);
+	//return(memory);
 }
 DLL_PLATFORMSUPPORT_EXPORTS void CDECL my_operator_delete_replacement(void* memory)
 {
-	if (memory != nullptr)
-		free(memory);
+	//if (memory != nullptr)
+	//	free(memory);
+}
+
+DLL_PLATFORMSUPPORT_EXPORTS void PlatformSetup()
+{
+	PlatformMemorySetup();
+}
+
+void PlatformMemorySetup()
+{
+	if (memory == nullptr) {
+		memory = (char*)malloc(MEMORY_SIZE);
+		memorySize = MEMORY_SIZE;
+		memoryUsed = 0;
+	}
+	currentMemory = memory;
 }
--- PlatformSupport/PlatformSupport.h
+++ PlatformSupport/PlatformSupport.h
@@ -1,3 +1,20 @@
+// Copyright (c) 2019 LLambert
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
 #pragma once
 #ifdef WIN32
 #pragma warning( disable : 4244 )
@@ -20,10 +37,42 @@
 #include <stdint.h>
 #include <cstdio>
 #include <new>
+/*!
+ * Align a value to the a certain size.
+ * For example double. 
+ */
+#define	Align(a,b) ((a + (b - 1)) & ~(b - 1))
+
+/*!
+ * Platform Replacement for new operator
+ * The current implementation is not thread safe.
+ * It is meant to recycle the memory between executions.
+ * This means there can be no persisten items between executions.
+ * All presistent items must be handled in the library suppory data access area.
+ * 
+ * \param size required
+ * \return pointer to memory or nullptr
+ */
 DLL_PLATFORMSUPPORT_EXPORTS void* CDECL my_operator_new_replacement(size_t size);
+
+/*!
+ * free up a piece of memory.
+ * Currently this does nothing.
+ * 
+ * \param memory
+ */
 DLL_PLATFORMSUPPORT_EXPORTS void CDECL my_operator_delete_replacement(void* memory);
 
 inline void* operator new  (size_t size) {return(my_operator_new_replacement(size));}
 inline void* operator new[]  (size_t size) { return(my_operator_new_replacement(size)); }
 inline void	operator	delete(void* memory) { my_operator_delete_replacement(memory); }
 inline void	operator	delete[](void* memory) { my_operator_delete_replacement(memory); }
+
+/*!
+ * Handle platform setup tasks before start of execution.
+ */
+DLL_PLATFORMSUPPORT_EXPORTS void	PlatformSetup();
+/*!
+ * Handle memory setup tasks before start of execution.
+ */
+void	PlatformMemorySetup();
